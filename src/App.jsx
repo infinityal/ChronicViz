744f0d07a9523ddedebf7770d200991d951130cc
infinityal <zhjzh15@uw.edu>
2022-01-21 19:50:37 -0800

    code draft 1 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 'use strict';
 
 import './App.css'
-import * as d3 from "d3";
-import { useState, useEffect } from 'react';
+
 import BarChart from './BarChart';
+import LineChart from './LineChart';
   
 function App() {
 
@@ -13,8 +13,9 @@ function App() {
         <h1>World Population Visualization 1950 - 2100</h1>
       </div>
       <BarChart />
+      <LineChart />
     </div>
   )
-}
+} 
 
 export default App
